fb37d1626c66a4f64adb2904b156c854036264e7
saltedfish <yushengyun2013@163.com>
2021-04-07 22:51:36 +0800

    Go Funtions

--- basic/functions.go
+++ basic/functions.go
@@ -10,7 +10,27 @@ func add1(x, y int) int {
 	return x + y
 }
 
+/*
+返回多个值
+*/
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+/**
+命名返回值
+*/
+func split(sum int) (x, y int) {
+	x = sum / 10
+	y = sum % 10
+	return
+}
+
 func main() {
 	fmt.Println(add(1, 2))
 	fmt.Println(add1(1, 3))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	x, y := split(12)
+	fmt.Println(x, y)
 }
